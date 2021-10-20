729e95059fd7346761514c7f0ce0a45c6ffdefc1
Elen Sukiasyan <elen.sukiasyan.2003@gmail.com>
2021-10-21 00:21:02 +0400

    created problem 1's solution

---
+++ JSProblems/elen.js
@@ -0,0 +1,12 @@
+const theight = 6;
+const tbase = 7;
+const rlenght = 3;
+const rwaight = 8;
+let tarea = theight*tbase/2;
+let rarea = rlenght*rwaight;
+if (tarea > rarea)
+{
+    console.log ("The triangle has a bigger area")
+}
+else
+console.log ("The rectangle has a bigger area")
